d47a448ea64233a1ee2e94783d7bcbbd3a96081b
harshadakadam74 <kadamharshada@gmail.com>
2026-06-10 19:13:19 +0530

    app.js and index

--- src/index.js
+++ src/index.js
@@ -16,3 +16,14 @@ connectDB();
 //         throw error;
 //     }
 // })()
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MONGO db connection failed !!!",error);
+    
+})
