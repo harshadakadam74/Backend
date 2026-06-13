bbc317b690d2b28921e8204c6740e32de60714d2
harshadakadam74 <kadamharshada@gmail.com>
2026-06-13 21:19:20 +0530

    app.js

--- src/app.js
+++ src/app.js
@@ -10,6 +10,15 @@ app.use(cors({
 }))
 
 app.use(express.json({limit: "20kb"}))
-app.use(express.urlencoded({extended}))
- 
+app.use(express.urlencoded({extended: true, limit: "20kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+//routes import
+import userRouter from './routes/user.route.js'
+
+//routers declaration
+app.use("/api/v1/users", userRouter)
+
+
 export { app }
